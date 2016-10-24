58ad791136defb909d3b2a4a3a779076d887bc0a
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-24 13:59:58 -0500

    started edit page for posts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,26 +4,23 @@
 
 <div class="container">
 	<h1>Show Post</h1>
+	<img src="http://fillmurray.com/150/100">
+	<h1>Employer Name</h1>
+
+{{-- @include('layouts.partials.skills') --}}
+
 	<div class='container'>
-	<div class="row table well">
+		<div class="row table well">
+
+			<form class="form" method="POST" action="">
+				{!! csrf_field() !!}
+				{!! method_field('PUT') !!}
+				Content: <textarea class="form-control" name="description" rows="5" cols="40" ></textarea>
+				Location: <input class="form-control" type="text" name="location" value="">
+				<input class="btn-success btn" type="submit">
+			</form>
 	
-		<form >	
-
-		</form>
-	
-
-
-
-		<tr>
-			<div class="col-sm-12 text-center">
-			<img class="pull-left" src="http://fillmurray.com/150/100" />
-				<p>Employer Name</p>
-				<p>Project Description</p>
-				<p>Location</p>
-				<p class="pull-right">Most Recent</p>
-			</div>
-		</tr>
-	</div>
+		</div>
 	</div>
 </div>
 	
