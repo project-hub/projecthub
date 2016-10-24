96f228998373438e4f9b11aae4fed1ddf19329e6
htrevino29 <htrevino29@gmail.com>
2016-10-24 16:40:04 -0500

    added font-awesome link to head. fixed social media icons on footer.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,20 +1,21 @@
 <footer>
+    <div class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4 footercontent">
                     <span class="copyright">Copyright &copy; Project Hub 2016</span>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 footercontent">
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
+                        <li><a href="#"><i class="fa fa-twitter socialicon"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        <li><a href="#"><i class="fa fa-facebook socialicon"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                        <li><a href="#"><i class="fa fa-linkedin socialicon"></i></a>
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 footercontent">
                     <ul class="list-inline quicklinks">
                         <li><a href="#">Privacy Policy</a>
                         </li>
@@ -24,4 +25,10 @@
                 </div>
             </div>
         </div>
+    </div>
 </footer>
+
+
+
+
+
