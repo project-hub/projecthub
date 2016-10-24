8986d4ada325944bc4f244aa0c1f2cb9025f808e
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-24 11:33:32 -0500

    Started mockup of posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,37 @@
 <div class="container">
 	<h1>Posts</h1>
 	<div class='container'>
+	<div class="row table well">
+		<tr>
+			<div class="col-sm-12 text-center">
+				<p>Employer Name</p>
+				<p>Project Description</p>
+				<p>Location</p>
+				<p class="pull-right">Most Recent</p>
+			</div>
+		</tr>
+	</div>
+	<div class="row table well">
+		<tr>
+			<div class="col-sm-12 text-center">
+				<p>Employer Name</p>
+				<p>Project Description</p>
+				<p>Location</p>
+				<p class="pull-right">Created_at</p>
+			</div>
+		</tr>
+	</div>
+	<div class="row table well">
+		<tr>
+			<div class="col-sm-12 text-center">
+				<p>Employer Name</p>
+				<p>Project Description</p>
+				<p>Location</p>
+				<p class="pull-right">Created_at</p>
+			</div>
+		</tr>
+	</div>
+
 	</div>
 </div>
 	
