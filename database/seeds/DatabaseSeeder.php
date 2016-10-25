f111a5162d88445cf03f0b37fe0d371c95ff01a7
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-25 14:37:30 -0500

    finished Skills, Posts, and Users database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(SkillSeeder::class);
 
 
         Model::reguard();
