218a88c84b366bb4ac9c4a29100e78c616da4884
htrevino29 <htrevino29@gmail.com>
2016-10-25 15:58:07 -0500

    contact user modal complete contact us modal on footer complete

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -15,8 +15,7 @@
             </div>
             <div class="col-md-4">
                 <h3>Name: </h3>
-                <h4>Rating: </h4>
-                <h4><a href="">link to users email</a></h4>
+                <h4>Rating: </h4> 
                 <h4>Address:</h4>
                 <h4>Phone: </h4>
             </div>
@@ -24,6 +23,114 @@
             	<h4>LinkedIn:</h4>
             	<h4>Github:</h4>
             	<h4>Other: </h4>
+              <!-- Email Button trigger modal -->
+              <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal1">
+               <i class="fa fa-envelope" aria-hidden="true"></i> 
+              </button>
+              <!-- Modal -->
+              <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      {{-- -------------- EMAIL MODAL ------------------ --}}
+                      <h4 class="modal-title" id="myModalLabel">Send Message</h4>
+                    </div>
+                    <div class="modal-body">
+                      {{-- -----body-- --}}
+
+
+                    <div class="container">
+                      <div class="row">
+                          <div class="col-md-6 ">
+                            <div class="well well-sm">
+                              <form class="form-horizontal" action="http://formspree.io/htrevino29@gmail.com" method="post">
+                              <fieldset>
+                                <legend class="text-center">Email</legend>
+                        
+                                <!-- Name input-->
+                                <div class="form-group">
+                                  <label class="col-md-3 control-label" for="name">Name</label>
+                                  <div class="col-md-9">
+                                    <input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+                                  </div>
+                                </div>
+                        
+                                <!-- Email input-->
+                                <div class="form-group">
+                                  <label class="col-md-3 control-label" for="email">Your E-mail</label>
+                                  <div class="col-md-9">
+                                    <input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+                                  </div>
+                                </div>
+                        
+                                <!-- Message body -->
+                                <div class="form-group">
+                                  <label class="col-md-3 control-label" for="message">Your message</label>
+                                  <div class="col-md-9">
+                                    <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+                                  </div>
+                                </div>
+                        
+                                <!-- Form actions -->
+                                <div class="form-group">
+                                  <div class="col-md-12 text-right">
+                                    <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                                  </div>
+                                </div>
+                              </fieldset>
+                              </form>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+
+
+                      
+                      {{-- ---end modal body------ --}}
+                    
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 {{-- ------------------- --}}
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
