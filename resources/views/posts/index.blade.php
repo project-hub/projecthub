423d8a87da0af97f6e012629d350ead4d071ced0
htrevino29 <htrevino29@gmail.com>
2016-10-28 11:17:03 -0500

    updated company post index to match users index.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,39 +4,31 @@
 
 <div class="container">
 	
-{{--  ******************** SKILLS SELECT TABLE *************************** --}}
-<div>	
-	@include('layouts.partials.skills', ['skills'=>$skills])
-</div>
-{{--  ******************** END SKILLS SELECT TABLE *************************** --}}
-
-
-	<h1>Posts</h1>
-	<div class='container'>
-
-	@foreach($posts as $post)
-
-		<div class="row">
-			<tr>
-				<div class="col-sm-12 text-center">
-				<img class="pull-left" src="http://fillmurray.com/150/100" />
-					<p>Employer Name</p>
-					<a href="{{ action('PostsController@show', $post->id) }}"><p>Title</p></a>
-					<p>{{ $post->title }}</p>
-					<p>Project Description</p>
-					<p>{{ $post->content }}</p>
-					<p>Location</p>
-					<p class="pull-right">{{ $post->created_at->diffForHumans() }}</p>
-				</div>
-			</tr>
-		</div>
-
-		
-
-	
-	@endforeach
-	{!! $posts->render() !!}
+	{{--  ******************** SKILLS SELECT TABLE *************************** --}}
+	<div>	
+		@include('layouts.partials.skills', ['skills'=>$skills])
 	</div>
+	{{--  ******************** END SKILLS SELECT TABLE *************************** --}}
+
+	<h1>Project Posts</h1>
+	<hr>
+	@foreach($posts as $post)
+	<div class="row">		
+		<div class="col-sm-4">
+			<img class=" img-responsive" src="http://fillmurray.com/300/300" />
+		</div>
+		<div class="col-sm-8">
+			<h4>Employer Name</h4>
+			<a href="{{ action('PostsController@show', $post->id) }}"><h4>{{ $post->title }}</h4></a>
+			<p>{{ $post->content }}</p>
+			<p>Location</p>
+			<p class="pull-right">{{ $post->created_at->diffForHumans() }}</p>
+			<a class="btn btn-default" href="{{ action('PostsController@show', $post->id) }}" role="button">See More</a>
+		</div>	
+	</div>
+	<hr>
+	@endforeach
+	{!! $posts->render() !!}	
 </div>
-	
+
 @stop
