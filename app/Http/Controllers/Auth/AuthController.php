68f83ed22cf80cf86cfdd1bf5d057b0388f18a84
Dewayne Allen <dewayneallen76@gmail.com>
2016-10-31 16:24:21 -0500

    added change password to the edit profile blade which will take the user to the page to enter a password to get a link to reset

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -51,6 +51,8 @@ class AuthController extends Controller
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
+
+        $request->session()->flash('SUCCESS_MESSAGE', 'User created successfully! Please complete your profile');
     }
 
     /**
