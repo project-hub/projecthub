392f453aaa370494b90f4d456c522f9bdff7a882
Dewayne Allen <dewayneallen76@gmail.com>
2016-11-01 10:42:16 -0500

    added rules for sorting views if user is logged in as an employer or a developer

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,6 +9,29 @@
     </div>
 </div>
 @foreach($users as $user)
+@if(Auth::user()->employer == 0 && $user->employer == 1)
+<div class="row">
+    <div class="col-md-4">
+        <a href="">
+            <img class="userPic img-responsive" src="{{ $user->image }}" alt="User Profile" height="" width="">
+        </a>
+            <h5>Skills Needed: </h5>
+            @foreach($user->skills as $skill)
+            <span class="badge">{{$skill->name}}</span>
+            @endforeach
+    </div>
+    <div class="col-md-8">
+        <h4 class="name postTitle">{{ $user->first_name . " " . $user->last_name }}</h4>
+        <h4>{{ $user->company_name }}</h4>
+            <h5><a href="{{ $user->email }}">{{ $user->email }}</a></h45>
+            <h5>Linked In: <a href="{{ $user->linkedin_id }}">{{ $user->linkedin_id }}</a></h5>
+            <h5>Website: <a href="{{ $user->website }}">{{ $user->website }}</a></h5>
+            <p class="welcomeText">{{ $user->content }}</p>
+            <a class="btn btn-default" href="{{ action('UsersController@show', $user->id) }}">View Profile <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+    </div>
+    <hr>
+@elseif(Auth::user()->employer == 1 && $user->employer == 0)
 <div class="row">
     <div class="col-md-4">
         <a href="">
@@ -21,26 +44,17 @@
     </div>
     <div class="col-md-8">
         <h4 class="name postTitle">{{ $user->first_name . " " . $user->last_name }}</h4>
-        @if($user->employer == 1)
-        <h4>{{ $user->company_name }}</h4>
-        @endif
-
             <h5><a href="{{ $user->email }}">{{ $user->email }}</a></h45>
-
             <h5>Linked In: <a href="{{ $user->linkedin_id }}">{{ $user->linkedin_id }}</a></h5>
-
-            @if($user->employer == 0)
             <h5>Github: <a href="{{ $user->github }}">{{ $user->github }}</a></h5>
-            @endif
-
             <h5>Website: <a href="{{ $user->website }}">{{ $user->website }}</a></h5>
-
             <p class="welcomeText">{{ $user->content }}</p>
             <a class="btn btn-default" href="{{ action('UsersController@show', $user->id) }}">View Profile <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
     </div>
     <hr>
-    @endforeach
+@endif
+@endforeach
 </div>
 <div class="text-center">{!! $users->render() !!} </div>
 </div>
